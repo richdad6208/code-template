d1f5958bda6419be8675d7a626b96bb9b3dd062e
Jaesung Park <richdad6208@gmail.com>
2025-06-10 19:17:33 +0900

    Feat: 픽원 타입 추가

--- src/domain/portal/main/main.tsx
+++ src/domain/portal/main/main.tsx
@@ -1,8 +1,26 @@
 import useToast from "@/src/hooks/use-toast";
+import { PickOne } from "@/src/type/utility-types";
 import { Button } from "@mantine/core";
+
+type Card = {
+  name: string;
+};
+
+type Bard = {
+  age: string;
+};
+
+type Total = PickOne<Bard & Card>;
 
 export default function Main() {
   const { success } = useToast();
+
+  const a = (o: Total) => {
+    console.log(o.age);
+    if (o.name) {
+      console.log("dd");
+    }
+  };
 
   return (
     <div className="">
